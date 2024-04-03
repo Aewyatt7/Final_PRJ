17e305a2ea231e6a700a33e98de367abcee08755
Andrew Wyatt <aewyatt7@gmail.com>
2024-04-03 11:15:18 -0600

    FinalPRJ

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+        <hr></hr>
+      <p> &copy; {new Date().getFullYear()} NBA & NCAA Bets</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/GambleSite.jsx
+++ src/components/GambleSite.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
-const sportsbooks =["DraftKings","BetMGM","FanDuel"];
+const sportsbooks = ["DraftKings", "BetMGM", "FanDuel"];
+
+const sportsConfig = {
+  Basketball: {
+    api: ["basketball_nba", "basketball_ncaab"],
+  },
+};
 
 const GambleSite = () => {
   const [sportsData, setSportsData] = useState([]);
   const [filteredSportsData, setFilteredSportsData] = useState([]);
   const [selectedSport, setSelectedSport] = useState([]);
-  const [gamesWanted, setGamesWanted] = useState([])
+  const [gamesWanted, setGamesWanted] = useState([]);
   const [oddsData, setOddsData] = useState([]);
 
-  const apiKey = "05abdb2810a898d570328f0ff0ad3581";
+  const apiKey = "18100a7eb1ac101521815a7ed8f9ce3c";
   const oddsApiUrl = "https://api.the-odds-api.com/v4";
   const sportsWanted = [
     //"American Football",
@@ -21,10 +27,11 @@ const GambleSite = () => {
     //"Golf",
   ];
 
-// multple fetches at the "same time"
-//and then handle all of their resolutions
+  // multple fetches at the "same time"
+  //and then handle all of their resolutions
 
-
+  {
+  }
 
   useEffect(() => {
     const fetchSportsData = async () => {
@@ -32,11 +39,10 @@ const GambleSite = () => {
         const response = await fetch(`${oddsApiUrl}/sports/?apiKey=${apiKey}`);
 
         const data = await response.json();
-        console.log(data)
+        console.log(data);
         //reconsider using this maybe we dont need the setsportsData
         setSportsData(data);
-        console.log(sportsData)
-
+        console.log(sportsData);
       } catch (error) {
         console.error("Error fetching sports data:", error);
       }
@@ -45,7 +51,6 @@ const GambleSite = () => {
     fetchSportsData();
   }, []);
 
-
   useEffect(() => {
     //This will run after any time the user picks a new sport in the dropdown
     //We want to fetch all the markets data at once
@@ -53,51 +58,49 @@ const GambleSite = () => {
     //We want to get the market data FOR EACH LEAGUE
 
     const fetchPromises = gamesWanted.map((sportLeague) => {
-        //You can list multiple markets as a API parameter later when you want to change this
-        return fetch(`${oddsApiUrl}/sports/${sportLeague.key}/odds?apiKey=${apiKey}&regions=us&markets=h2h,spreads,totals`);
+      //You can list multiple markets as a API parameter later when you want to change this
+      return fetch(
+        `${oddsApiUrl}/sports/${sportLeague.key}/odds?apiKey=${apiKey}&regions=us&markets=h2h,spreads,totals`
+      );
     });
 
     //Bundles all your promises and consumes all of them
     Promise.all(fetchPromises)
-        .then((responsesArray) => {
-            return Promise.all(responsesArray.map((response) => response.json()));
+      .then((responsesArray) => {
+        return Promise.all(responsesArray.map((response) => response.json()));
+      })
+      .then((dataArray) => {
+        console.log("betting odds data top level array", dataArray);
 
-        })
-        .then((dataArray) => {
-            console.log('betting odds data top level array', dataArray);
+        // Set the state variable to the dataArray variable
+        const filterBooks = dataArray
+          .flat()
+          .map((game) => game.bookmakers)
+          .map((bookmakers) =>
+            bookmakers.filter((book) => sportsbooks.includes(book.title))
+          )
+          .flat();
+        setOddsData(dataArray);
+        /*setOddsData(filterBooks);*/
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [gamesWanted]);
 
-            // Set the state variable to the dataArray variable
-            setOddsData(dataArray);
+  console.log(sportsbooks);
 
-            const basketballNBA = dataArray[1];
-            basketballNBA.forEach(obj => {
-                const id = obj.id;
-                const sportKey = obj.sport_key;
-                const sportTitle = obj.sport_title;
-                const commenceTime = obj.commence_time;
-                const homeTeam = obj.home_team;
-                const awayTeam = obj.away_team;
-                const bookMakers = obj.bookMakers;
-              
-
-                console.log(`ID: ${id}, Sport Key: ${sportKey}, Sport Title: ${sportTitle}, Commence Time: ${commenceTime}, Home Team: ${homeTeam}`);
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-}, [gamesWanted]);
-
-
-  
-console.log(sportsbooks)
-console.log(oddsData[1].map(thing2 => thing2.bookmakers).filter(thing => {
-  console.log(thing.title, thing)
-  return sportsbooks.includes(thing.title)}))
+  // console.log(oddsData[1]?.map(game => game.bookmakers).filter(bookmakers => {
+  //   console.log(bookmakers.title, bookmakers)
+  //   return sportsbooks.includes(bookmakers.title)
+  // }))
   return (
     <div>
-      <h2>All Sports Available for Betting</h2>
-      <select onChange={handleSportChange} value={selectedSport.length > 0 ? selectedSport[0].key : ""}>
+      <h2>Today's Games</h2>
+      <select
+        onChange={handleSportChange}
+        value={selectedSport.length > 0 ? selectedSport[0].key : ""}
+      >
         <option>Select a sport</option>
         {sportsWanted.map((sport, index) => (
           <option key={index} value={sport}>
@@ -106,43 +109,113 @@ console.log(oddsData[1].map(thing2 => thing2.bookmakers).filter(thing => {
         ))}
       </select>
       <div>
-        {oddsData[1].map(thing2 => thing2.bookmakers).filter(thing => sportsbooks.includes(thing.title)).map((obj) => {
-          return(
-            <ul>{obj.markets.map(market => {
-              return <li>{market.outcomes.map(outcome => { 
-                return <p>{outcome.name}:{outcome.price}</p>
-              })}</li>
-            })}</ul>
-            
-          )
-        })}
-        
-      </div>
-      <div>
-        <p>Games </p>
+        {/* oddsData - []
+          -> game.bookmakers - map [[{}], [{}], [{}], [{}], [{}], [{}, {}, {}]]
+          -> bookmakers - map [{}, {}, {}] -> filter book.title [{}, {}, {}] - [[{}], [{}], [{}], [{}], [{}], [{}, {}, {}]]
+          -> flat - [{}, {}, {}, {}, {}, {}, {}]
+        */}
+        <table>
+          <tbody>
+            {oddsData.length > 0 &&
+              oddsData[0].map((obj, idx) => {
+                return (
+                  <table border="1">
+                    <tr align='center'>
+                      <td>{obj.away_team} @ {obj.home_team}</td>
+                    </tr>
+                    {obj.bookmakers.map((bookmaker) => {
+                      return (
+                        <tr align='center'>
+                          <td>{bookmaker.title}</td>
+                          {bookmaker.markets.map((market) => {
+                            return <td>{market.key}</td>;
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </table>
+                );
+              })}
+            {/*
+              obj.markets.map((market, marketIdx) => {
+                if (market.key == "h2h") {
+                  return <div>h2h</div>;
+                }
+                if (market.key == "spreads") {
+                  return <div>spreads</div>;
+                }
+                if (market.key == "totals") {
+                  return <div>totals</div>;
+                }
+
+                return (
+                  <tr key={`${idx}-${marketIdx}-${outcomeIdx}`}>
+                    <td>{outcome.name}</td>
+                   
+                    <td>
+                      <table>
+                        <tr>
+                          <th></th> 
+                          <th>FanDuel</th>
+                          <th>MGM</th>
+                          <th>Draft Kings</th>
+                        </tr>
+                        <tr>
+                          {" "}
+                         
+                          <th>ML:</th>
+                          <td>DATA</td>
+                          <td>{outcome.point}</td>
+                          <td>DATA</td>
+                        </tr>
+                        <tr>
+                          {" "}
+                          
+                          <th>Spread:</th>
+                          <td>{outcome.price}</td>
+                          <td>DATA</td>
+                          <td>DATA</td>
+                        </tr>
+                        <tr>
+                          {" "}
+                          
+                          <th>Total:</th>
+                          <td>DATA</td>
+                          <td>DATA</td>
+                          <td>DATA</td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                );
+              })
+            )}*/}
+          </tbody>
+        </table>
       </div>
     </div>
   );
-  
 
   function handleSportChange(event) {
     const selectedSport = event.target.value;
     setSelectedSport(selectedSport);
     console.log("selected sport", selectedSport);
 
-    const tempGames = sportsData.filter((sport) =>
-      selectedSport.includes(sport.group)
+    const tempGames = sportsData.filter(
+      (sport) =>
+        selectedSport.includes(sport.group) &&
+        sportsConfig[selectedSport].api.includes(sport.key)
     );
     console.log("sorting practice", tempGames);
-      setGamesWanted (tempGames);
+    setGamesWanted(tempGames);
     console.log("Temp League:", tempGames);
   }
 };
 
 export default GambleSite;
 
-
 // look up how to iterate over react  promised objects???
 //XML format... learn about it
 //stockdash
-
+// merge the data... after making the data
+// How to sort an array in javascript
